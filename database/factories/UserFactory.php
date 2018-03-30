1d0071dcf81249c1c0e32325d67fcdf34599f9a6
Byron Machin <bmachin7@gmail.com>
2018-03-30 16:02:06 -0400

    Created factories and seeders for database migrations.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Category;
+use App\Product;
+use App\Seller;
+use App\Transaction;
+use App\User;
 use Faker\Generator as Faker;
 
 /*
@@ -13,11 +18,45 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        // 'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => $faker->password ?: $faker->password = bcrypt('secret'),
+        'verificated' => $verificated = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verificated == User::VERIFIED_USER ? null : User::generateVerificationToken(),
+        'admin' => $admin = $faker->randomElements([User::ADMIN_USER, User::REGULAR_USER]),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::PRODUCT_AVAILABLE, Product::PRODUCT_UNAVAILABLE]),
+        'image' => $faker->randomElement(['watch.jpg', 'smartphone.jpg', 'laptop.jpg']),
+        'seller_id' => User::all()->first()->id(),
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+    return [
+        'name' => $faker->word,
+        'quantity' => $faker->numberBetween(1, $seller->quantity),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
